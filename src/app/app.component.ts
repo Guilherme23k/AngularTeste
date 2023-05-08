0a115d60cd552bcfac6f7b2ed11d7a192a68e668
guilherme <guilhermeggr23@gmail.com>
2023-05-08 18:52:59 -0300

    Resolução Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cepConsult';
+
+  cep: string = '';
+  endereco: any;
+
+  pesquisado = false;
+  espera = false;
+
+  constructor(private http: HttpClient) {}
+
+  pesquisarCep() {
+
+    this.espera = true;
+
+    this.cep=this.cep.replace(/\D/g,'')
+    this.http.get(`https://viacep.com.br/ws/${this.cep}/json/`).subscribe((endereco: any) => {
+      this.endereco = endereco;
+      this.pesquisado=true;
+      this.espera=false;
+    });
+  }
+
+  limpa(){
+    this.endereco = '';
+    this.pesquisado = false;
+  }
 }
